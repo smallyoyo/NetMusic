dc6819d9347dcd7af6a2a956b54ac9c3bbe09f4e
smallyoyo <1421296065@qq.com>
2018-07-09 23:18:07 +0800

    first commit

---
+++ src/common/js/jsonp.js
@@ -0,0 +1,23 @@
+import orignJSONP from 'jsonp'
+
+export default function jsonp (url, data, option) {
+  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
+  return Promise((resolve, reject) => {
+    orignJSONP(url, option, (err, data) => {
+      if (!err) {
+        resolve(data)
+      } else {
+        reject(err)
+      }
+    })
+  })
+}
+
+function param (data) {
+  let url = ''
+  for (var k in data) {
+    let value = data[k] !== undefined ? data[k] : ''
+    url += `&${k}=${encodeURIComponent(value)}`
+  }
+  return url ? url.substring(1) : ''
+}
---
+++ src/components/rank/rank.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>排行页</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
+
